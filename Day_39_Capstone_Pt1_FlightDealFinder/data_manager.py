71fa218110dc82abb12dd3a0f801fc5c98a76217
asalamatov <azamat.salamatov2004@gmail.com>
2023-02-18 13:11:49 -0600

    added day 39 still have problem with accessing google sheets

--- Day_39_Capstone_Pt1_FlightDealFinder/data_manager.py
+++ Day_39_Capstone_Pt1_FlightDealFinder/data_manager.py
@@ -1,3 +1,28 @@
+import requests
+
+sheety_endpoint = "https://api.sheety.co/8770d1eda53a01f2846505862b8a3d48/flightDeals/prices"
+
+headers = {
+    "Authorization": "Bearer qwertyuiopasdfghjklzxcvbnm",
+}
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    
+    def __init__(self) -> None:
+        self.destination_data = {}
+        
+    def get_destionation_data(self):
+        data = requests.get(url=f"{sheety_endpoint}", headers=headers).json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+    
+    
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "prices": {
+                    "iataCode":city["iataCode"]
+                }
+            }
+            response = requests.put(url=f"{sheety_endpoint}/{city['id']}", json=new_data)
+            print(response)
