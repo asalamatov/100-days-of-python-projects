df4a2670f2a288c1cb29c7b67ae1810b34ca6fa6
asalamatov <azamat.salamatov2004@gmail.com>
2023-02-18 13:19:23 -0600

    peredumal

--- Day_39_Capstone_Pt1_FlightDealFinder/main.py
+++ Day_39_Capstone_Pt1_FlightDealFinder/main.py
@@ -1,4 +1,5 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import requests
 from flight_search import FlightSearch
 from data_manager import DataManager
 
