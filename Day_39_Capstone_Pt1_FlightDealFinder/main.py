dd1c5da899e8d50f38c7ad579a34e5139271ec6e
asalamatov <azamat.salamatov2004@gmail.com>
2023-02-18 13:17:22 -0600

    corrected code

--- Day_39_Capstone_Pt1_FlightDealFinder/main.py
+++ Day_39_Capstone_Pt1_FlightDealFinder/main.py
@@ -1,6 +1,4 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-import requests
-from pprint import pprint
 from flight_search import FlightSearch
 from data_manager import DataManager
 
